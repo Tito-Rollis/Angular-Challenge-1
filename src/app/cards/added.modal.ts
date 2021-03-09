2775817fb92814d0615971eb5b5ea2aa2af1486e
Tito-Rollis <ouvalres@gmail.com>
2021-03-09 16:20:02 +0700

    Still stuck at added to chart button

---
+++ src/app/cards/added.modal.ts
@@ -0,0 +1,8 @@
+export class Added {
+    name: string;
+    price: number;
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
---
+++ src/app/cards/card.model.ts
@@ -0,0 +1,13 @@
+export class Cards {
+    public name: string;
+    public img: string;
+    public price: number;
+    public desc: string;
+    constructor(name, img, price, desc) {
+        this.name = name;
+        this.img = img;
+        this.price = price;
+        this.desc = desc;
+    }
+}
+
